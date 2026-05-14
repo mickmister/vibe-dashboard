2194ff7f640f4d9420db05aa934a525ab7f22b42
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2026-05-14 15:18:47 -0400

    change default VD port to 3005

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import { playwright } from '@vitest/browser-playwright';
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
-let serverPort = 1337;
+let serverPort = 3005;
 if (process.env.SERVER_PORT || process.env.PORT) {
   serverPort = parseInt(process.env.SERVER_PORT || process.env.PORT!);
 }
