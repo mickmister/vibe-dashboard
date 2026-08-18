7fab8e21e2357da9fb11f57039f13590aea4e673
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2026-08-18 13:37:24 -0400

    Revert "chore: format vibe kanban workspace proxy"
    This reverts commit 1280d994346d699e5b80199d42efc863c3eed786.

--- src/modules/plugins/vibe-kanban/module.ts
+++ src/modules/plugins/vibe-kanban/module.ts
@@ -59,7 +59,7 @@ import {serverRegistry} from 'springboard/server/register';
 serverRegistry.registerServerModule(({hono}) => {
   hono.get('/vk-api/workspaces', async c => {
     const url = `${process.env.VIBE_API_URL}/api/workspaces`;
-    return fetch(url);
+    return fetch(url)
   });
 })
 // @platform end
